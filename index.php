51fcd88d91446b4ac683304ff3cad7fb9d59d7c9
KVN <78144196+kvnpp007ss@users.noreply.github.com>
2024-06-14 12:15:55 -0700

    Ecologo commit

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <link rel="stylesheet" href="styles.css">
-    <link rel="icon" href="assets/img/tiburonBallena.jpg">
+    <link rel="icon" href="assets/logo-files/favicon-32x32.png">
     <title>Eco Turismo</title>
     </head>
     <body background="assets/img/morgan-newnham.jpg">
         <!-- Encabezado -->
         <header>
+            <img src="assets/logo-files/logo.png" alt="Logo Eco Turismo">
         </header>
         <main>
             <h1>¡Hola, Pakito!</h1>
