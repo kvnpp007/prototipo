65700f7c210fa8071c777fe54a4497099ac5bb8c
KVN <78144196+kvnpp007ss@users.noreply.github.com>
2024-06-18 22:14:47 -0700

    Eliminar title extra

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <!DOCTYPE HTML>
-<!--
-
--->
 <html>
 	<head>
-		<title>Striped by HTML5 UP</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<!-- Titulos de la página -->
